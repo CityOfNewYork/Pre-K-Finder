2cd8b893190c91d88f22ac481d0c948033b3cd46
timkeane <tkeane@doitt.nyc.gov>
2015-02-18 16:14:01 -0500

    Never fear! SSL is here!

--- upk/src/main/webapp/js/nyc/constants.js
+++ upk/src/main/webapp/js/nyc/constants.js
@@ -13,7 +13,7 @@ var	RESOLUTIONS = [434.027777777778, 303.819444444444, 222.222222222222, 111.111
 	 * change the appropriate values below
 	 * 
 	 */
-	APPLICATION_URL = document.domain == "maps.nyc.gov" ? "//schools.nyc.gov/schoolsearch/apply.html" : "//schoolsstg.nycenet.edu/schoolsearch/apply.html",
+	APPLICATION_URL = document.domain == "maps.nyc.gov" ? "//www.nycenet.edu/schoolsearch/apply.html" : "http://schoolsstg.nycenet.edu/schoolsearch/apply.html",
 	/* 
 	 * un-comment to revert to DOE form	if they decide not to use doitt replacement form
 	 * 
