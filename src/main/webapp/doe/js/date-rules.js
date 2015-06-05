60c67531acc23926b487bf60cfa1b91616e61dc9
timkeane <tkeane@doitt.nyc.gov>
2015-06-05 12:01:08 -0400

    new messages

--- src/main/webapp/doe/js/date-rules.js
+++ src/main/webapp/doe/js/date-rules.js
@@ -9,9 +9,9 @@ var APPLY_END_DATE = "2015-07-11";
 
 /********** Pre-K Finder Content **********/
 
-var SPLASH_MSG_NO_APPLY = "<div>All NYC children born in 2011 are eligible to attend pre-K in September 2015.</div><div>Complete the 'Get in Touch' form and an enrollment specialist will contact you with pre-K options for your child.</div>You can also apply in the Round 2 application period from June 22 - July 10, 2015. Round 2 will include new pre-K programs at district schools and NYC Early Education Centers.";
+var SPLASH_MSG_NO_APPLY = "<div>All NYC children born in 2011 are eligible to attend pre-K in September 2015.</div><div>Complete the <b>Get in Touch Form</b> and an enrollment specialist will contact you with pre-K options for your child.</div>You can also apply in the Round 2 application period from June 22 - July 10, 2015. Round 2 will include new pre-K programs at district schools and NYC Early Education Centers.";
 
-var SPLASH_MSG_YES_APPLY = "Some other message!";
+var SPLASH_MSG_YES_APPLY = "<div>All NYC children born in 2011 are eligible to attend pre-k in September 2015.</div><div>The Round 2 application period from June 22-July 10,2015 is now open. Round 2 includes new pre-k programs at district schools and NYC Early Education Centers.</div>To only view new programs that are currently accepting applications please use the Accepting Applications filter on the right-hand side of the map. To submit an application and rank these new programs, plese click <b>Apply Now</b>.";
 
 var SPLASH_INFO_BUTTON_TITLE_NO_APPLY = "Get in Touch";
 
@@ -25,7 +25,7 @@ var SCHOOL_YEAR = "2015-16";
 
 var FORM_MSG_NO_APPLY = "This form is only a statement of interest and not an application. Please only submit the form once per household. An Enrollment Specialist will contact you shortly. For now, please visit <a href='http://nyc.gov/prek' target='_blank'>nyc.gov/prek</a> for more information.";
 
-var FORM_MSG_YES_APPLY = "Some other message!";
+var FORM_MSG_YES_APPLY = "This form is only a statement of interest and not an application. Please only submit the form once per household. An Enrollment Specialist will contact you shortly. For now, please visit <a href='http://nyc.gov/prek' target='_blank'>nyc.gov/prek</a> for more information.";
 
 var MIN_DOB_YEAR = 2011;
 
