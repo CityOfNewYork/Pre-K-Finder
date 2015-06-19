3dc5f042b626b9e3103d78dca3b12c2315ed1b3a
timkeane <tkeane@doitt.nyc.gov>
2015-06-19 10:05:28 -0400

    latest data and messages

--- src/main/webapp/doe/js/date-rules.js
+++ src/main/webapp/doe/js/date-rules.js
@@ -11,7 +11,7 @@ var APPLY_END_DATE = "2015-07-11";
 
 var SPLASH_MSG_NO_APPLY = "<div>All NYC children born in 2011 are eligible to attend pre-K in September 2015.</div><div>Click &#34;<span class='capitalize'>call me</span>&#34; to hear from an enrollment specialist.</div>You can also apply in the <span class='capitalize'>round 2</span> application period from June 22 - July 10, 2015. <span class='capitalize'>round 2</span> will include new pre-K programs at district schools and NYC Early Education Centers.";
 
-var SPLASH_MSG_YES_APPLY = "<div>All NYC children born in 2011 are eligible to attend pre-K in September 2015.</div><div>Click &#34;<span class='capitalize'>call me</span>&#34;to hear from an enrollment specialist.</div><div>The <span class='capitalize'>main round</span> application period is now closed. You can still contact <span class='capitalize'>main round</span> programs directly to inquire about waitlists and availability.</div>You can still apply for pre-K in the <span class='capitalize'>round 2</span> application period! <span class='capitalize'>round 2</span> pre-K programs were not available during the <span class='capitalize'>main round</span>. Click the &#34;Continue to the map for <span class='capitalize'>round 2 programs</span>&#34; button below, to see the programs available in <span class='capitalize'>round 2</span>.";
+var SPLASH_MSG_YES_APPLY = "<div>All NYC children born in 2011 are eligible to attend pre-K in September 2015.</div><div>Click &#34;<span class='capitalize'>call me</span>&#34; to hear from an enrollment specialist.</div><div>The <span class='capitalize'>main round</span> application period is now closed. You can still contact <span class='capitalize'>main round</span> programs directly to inquire about waitlists and availability.</div>You can still apply for pre-K in the <span class='capitalize'>round 2</span> application period from June 22-July 10. <span class='capitalize'>round 2</span> programs include newly awarded NYCEEC pre-k programs, as well as other featured programs for this fall. Click the &#34;Continue to the map for <span class='capitalize'>round 2 programs</span>&#34; button below, to see the programs available in <span class='capitalize'>round 2</span>.";
 
 var SPLASH_INFO_BUTTON_TITLE_NO_APPLY = "<span class='capitalize'>call me</span>";
 
@@ -25,9 +25,9 @@ var SCHOOL_YEAR = "2015-16";
 
 /********** Get in Touch Form Content **********/
 
-var FORM_MSG_NO_APPLY = "This form is only a statement of interest and not an application. Please only submit the form once per household. An Enrollment Specialist will contact you shortly. For now, please visit <a href='http://nyc.gov/prek' target='_blank'>nyc.gov/prek</a> for more information.";
+var FORM_MSG_NO_APPLY = "This form is only a statement of interest and not an application. Please only submit the form once per household. An Enrollment Specialist will contact you shortly. Please visit <a href='http://nyc.gov/prek' target='_blank'>nyc.gov/prek</a> for more information.";
 
-var FORM_MSG_YES_APPLY = "This form is only a statement of interest and not an application. Please only submit the form once per household. An Enrollment Specialist will contact you shortly. For now, please visit <a href='http://nyc.gov/prek' target='_blank'>nyc.gov/prek</a> for more information.";
+var FORM_MSG_YES_APPLY = "This form is only a statement of interest and not an application. Please only submit the form once per household. An Enrollment Specialist will contact you shortly. Please visit <a href='http://nyc.gov/prek' target='_blank'>nyc.gov/prek</a> for more information.";
 
 var MIN_DOB_YEAR = 2011;
 
